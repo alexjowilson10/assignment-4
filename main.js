3d38c2831312534b0383ccb5a2200f925aa124da
Alex Wilson <wilso292@wwu.edu>
2021-07-11 11:45:47 -0700

    updated

--- main.js
+++ main.js
@@ -32,5 +32,23 @@ $(document).ready(function() {
         }
     });
 
+    // typing animation script 
+    var typed = new Typed(".typing", {
+        strings: ["Soccer Player &#9917;", "Computer Science Major &#128104;&#8205;&#128187;", "Messi fan &#127462;&#127479; &#127942;"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Soccer Player", "Computer Science Major &#128104;&#8205;&#128187;", "Messi fan &#127462;&#127479; &#127942;"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
 
 });
